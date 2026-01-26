074eb116d24cade79b8f34c2125c7b159d6e005e
YevheniiaZalevska <yevhenia05@gmail.com>
2026-01-26 12:13:30 +0100

    Create ContactButtons.jsx

---
+++ src/components/ContactButtons.jsx
@@ -0,0 +1,18 @@
+import { MessageCircle } from "lucide-react";
+import { CONTACTS } from "../data/content";
+
+export default function ContactButtons({ compact = false }) {
+  return (
+    <div className={compact ? "row mt12" : "row mt16"} style={{ flexWrap: "wrap" }}>
+      <a className={`btn ${compact ? "btnSmall" : ""}`} href={CONTACTS.viber.href}>
+        <MessageCircle size={16} /> {CONTACTS.viber.label}
+      </a>
+      <a className={`btn ${compact ? "btnSmall" : ""}`} href={CONTACTS.telegram.href} target="_blank" rel="noreferrer">
+        <MessageCircle size={16} /> {CONTACTS.telegram.label}
+      </a>
+      <a className={`btn ${compact ? "btnSmall" : ""}`} href={CONTACTS.facebook.href} target="_blank" rel="noreferrer">
+        <MessageCircle size={16} /> {CONTACTS.facebook.label}
+      </a>
+    </div>
+  );
+}
